5e1159aa18c65778bdecd1660dfa07ea27eaed83
Weikun Liang <weikunl@andrew.cmu.edu>
2015-03-13 20:51:04 -0400

    maps working with a marker at cmu

--- src/com/pittsburghparks/pghparks/Map.java
+++ src/com/pittsburghparks/pghparks/Map.java
@@ -1,7 +1,14 @@
 package com.pittsburghparks.pghparks;
 
-
 import com.actionbarsherlock.app.SherlockFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,11 +16,72 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Map extends SherlockFragment {
+	MapView mMapView;
+	private GoogleMap googleMap;
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.map, container, false);
-        return rootView;
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	        Bundle savedInstanceState) {
+	    // inflate and return the layout
+	    View v = inflater.inflate(R.layout.map, container,
+	            false);
+	    mMapView = (MapView) v.findViewById(R.id.mapView);
+	    mMapView.onCreate(savedInstanceState);
+
+	    mMapView.onResume();// needed to get the map to display immediately
+
+	    try {
+	        MapsInitializer.initialize(getActivity().getApplicationContext());
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    googleMap = mMapView.getMap();
+	    // latitude and longitude
+	    double latitude = 40.4433;
+	    double longitude = -79.9436;
+
+	    // create marker
+	    MarkerOptions marker = new MarkerOptions().position(
+	            new LatLng(latitude, longitude)).title("Hello Maps");
+
+	    // Changing marker icon
+	    marker.icon(BitmapDescriptorFactory
+	            .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+	    // adding marker
+	    googleMap.addMarker(marker);
+	    CameraPosition cameraPosition = new CameraPosition.Builder()
+	            .target(new LatLng(latitude, longitude)).zoom(12).build();
+	    googleMap.animateCamera(CameraUpdateFactory
+	            .newCameraPosition(cameraPosition));
+
+	    // Perform any camera updates here
+	    return v;
+	}
+
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    mMapView.onResume();
+	}
+
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    mMapView.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    mMapView.onDestroy();
+	}
+
+	@Override
+	public void onLowMemory() {
+	    super.onLowMemory();
+	    mMapView.onLowMemory();
+	}
 
 }
