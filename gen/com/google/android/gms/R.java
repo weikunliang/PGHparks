f45aa52b537a4cb9865e13123575a23691ce4ba2
Weikun Liang <weikunl@andrew.cmu.edu>
2015-04-04 19:44:31 -0400

    about page styled

--- gen/com/google/android/gms/R.java
+++ gen/com/google/android/gms/R.java
@@ -102,12 +102,12 @@ public final class R {
 		public static final int common_signin_btn_text_normal_light = 0x7f02009c;
 		public static final int common_signin_btn_text_pressed_dark = 0x7f02009d;
 		public static final int common_signin_btn_text_pressed_light = 0x7f02009e;
-		public static final int ic_plusone_medium_off_client = 0x7f0200a9;
-		public static final int ic_plusone_small_off_client = 0x7f0200aa;
-		public static final int ic_plusone_standard_off_client = 0x7f0200ab;
-		public static final int ic_plusone_tall_off_client = 0x7f0200ac;
-		public static final int powered_by_google_dark = 0x7f0200ca;
-		public static final int powered_by_google_light = 0x7f0200cb;
+		public static final int ic_plusone_medium_off_client = 0x7f0200aa;
+		public static final int ic_plusone_small_off_client = 0x7f0200ab;
+		public static final int ic_plusone_standard_off_client = 0x7f0200ac;
+		public static final int ic_plusone_tall_off_client = 0x7f0200ad;
+		public static final int powered_by_google_dark = 0x7f0200cd;
+		public static final int powered_by_google_light = 0x7f0200ce;
 	}
 	public static final class id {
 		public static final int adjust_height = 0x7f050012;
