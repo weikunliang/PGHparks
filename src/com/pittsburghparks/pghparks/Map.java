fb561506e2265889d39e7c9d44bc2f43ad53e482
Weikun Liang <weikunl@andrew.cmu.edu>
2015-04-14 17:36:14 -0400

    gps changed

--- src/com/pittsburghparks/pghparks/Map.java
+++ src/com/pittsburghparks/pghparks/Map.java
@@ -156,21 +156,20 @@ public class Map extends SherlockFragment implements LocationListener, LocationS
 	private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
 	    @Override
 	    public void onMyLocationChange(Location location) {
-
+	    	LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
 	    	if( mListener != null )
 	        {
 	            mListener.onLocationChanged(location);
 	            if(window == false){
-	            	googleMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+	            	googleMap.animateCamera(CameraUpdateFactory.newLatLng(current));
 	            }
 	        }
 	    	
-	        LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
-	        if(googleMap != null){
-	        	if(window == false){
-	        		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, 16.0f));
-	        	}
-	        }
+//	        if(googleMap != null){
+//	        	if(window == false){
+//	        		googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, 16.0f));
+//	        	}
+//	        }
 	    }
 	};
 	
@@ -235,17 +234,17 @@ public class Map extends SherlockFragment implements LocationListener, LocationS
     @Override
     public void onLocationChanged(Location location) 
     {
+    	coordinate = new LatLng(location.getLatitude(), location.getLongitude());
         if( mListener != null )
         {
             mListener.onLocationChanged(location);
  
-            googleMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLng(coordinate));
         }
   
-        coordinate = new LatLng(location.getLatitude(), location.getLongitude());
-        if(googleMap != null){
-        	googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 16.0f));
-        }
+//        if(googleMap != null){
+//        	googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 16.0f));
+//        }
     }
  
     @Override
